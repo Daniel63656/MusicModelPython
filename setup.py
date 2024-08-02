b71e729a9e21eaf8265e455da06d7006a2267c3f
Daniel <d.maier020@gmail.com>
2024-08-02 10:40:30 +0200

    added setup.py and empty readme

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='music_model',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Daniel63656/MusicModelPython',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,7 @@
 # This empty file is necessary to make imports work
-# To run a specific test from console and print its statements (from root directory):
-# $ pytest -s test/test_file.py::test_function
+"""
+To check wether tests compile:
+    $ pytest --collect-only
+To run a specific test from console and print its statements (from root directory):
+    $ pytest -s test/test_file.py::test_function
+"""
